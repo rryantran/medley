44b535d1f21dae7b749b26574197b5bdb142c056
ryantran <113406848+rryantran@users.noreply.github.com>
2024-03-24 00:32:49 -0500

    update dependencies and minor edits

--- medley-ui/src/App.jsx
+++ medley-ui/src/App.jsx
@@ -2,20 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [feedLink, setFeedLink] = useState(null);
-
-  useEffect(() => {
-    axios.get("/api/feedlinks/1").then((response) => {
-      console.log(response.data.url);
-      setFeedLink(response.data.url);
-    });
-  }, []);
-
-  return (
-    <>
-      <div>{feedLink}</div>
-    </>
-  );
 };
 
 export default App;
