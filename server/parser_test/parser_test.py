8eb653f22429b60c6a9ada692d7db99d7ad7b89e
ryantran <113406848+rryantran@users.noreply.github.com>
2024-06-02 14:43:12 -0500

    test feed parser

---
+++ server/parser_test/parser_test.py
@@ -0,0 +1,13 @@
+import feedparser
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+# parse the feed
+d = feedparser.parse(
+    'https://www.criterion.com/feeds/the-daily')
+
+# test output
+print(d.feed.title)
+print(d.entries[0].title)
