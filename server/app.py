91366d6a4196b3c121ba08e8dbb9de105990fcf9
ryantran <113406848+rryantran@users.noreply.github.com>
2024-06-02 19:48:16 -0500

    add serializers

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_migrate import Migrate
+from flask_restx import Api, Resource, fields
 from config import DevConfig
 from exts import db
 from models import User, Feed, Article
@@ -13,6 +14,34 @@ api = Api(app)
 
 # initlialize database
 db.init_app(app)
+
+# initialize migration
+migrate = Migrate(app, db)
+
+# user serializer
+user_model = api.model('User', {
+    'id': fields.Integer(),
+    'username': fields.String(),
+    'email': fields.String(),
+    'password': fields.String()
+
+})
+
+# feed serializer
+feed_model = api.model('Feed', {
+    'id': fields.Integer(),
+    'title': fields.String(),
+    'url': fields.String(),
+})
+
+# article serializer
+article_model = api.model('Article', {
+    'id': fields.Integer(),
+    'title': fields.String(),
+    'author': fields.String(),
+    'pub_date': fields.DateTime(),
+    'url': fields.String(),
+})
 
 
 # test route
