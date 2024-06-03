cd50d20e931f38e46a76d515013f6a435cc53f21
ryantran <113406848+rryantran@users.noreply.github.com>
2024-06-02 20:17:34 -0500

    add table relationships

--- server/models.py
+++ server/models.py
@@ -1,5 +1,9 @@
 from exts import db
 
+# user-feed association table
+user_feed = db.Table('user_feed', db.Column('user_id', db.Integer, db.ForeignKey(
+    'user.id')), db.Column('feed_id', db.Integer, db.ForeignKey('feed.id')))
+
 
 # user model
 class User(db.Model):
@@ -7,6 +11,8 @@ class User(db.Model):
     email = db.Column(db.String(254), unique=True, nullable=False)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.Text(), nullable=False)
+    feeds = db.relationship('Feed', secondary=user_feed,
+                            back_populates='users')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -17,6 +23,10 @@ class Feed(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
     url = db.Column(db.String(512), unique=True, nullable=False)
+    users = db.relationship('User', secondary=user_feed,
+                            back_populates='feeds')
+    articles = db.relationship(
+        'Article', back_populates='feed')
 
     def __repr__(self):
         return f'<Feed {self.title}>'
@@ -29,6 +39,8 @@ class Article(db.Model):
     author = db.Column(db.String(128), nullable=False)
     pub_date = db.Column(db.DateTime, nullable=False)
     url = db.Column(db.String(512), unique=True, nullable=False)
+    feed = db.relationship('Feed', back_populates='articles')
+    feed_id = db.Column(db.Integer, db.ForeignKey('feed.id'), nullable=False)
 
     def __repr__(self):
         return f'<Article {self.title}>'
