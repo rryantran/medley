1a242bbd81f198cef76a7e2ee41679cf07faf6cc
ryantran <113406848+rryantran@users.noreply.github.com>
2024-07-01 00:26:55 -0500

    fix import issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import NavBar from "./components/NavBar";
-import AddFeed from "./pages/AddFeed";
 import Articles from "./pages/Articles";
 import Feeds from "./pages/Feeds";
 import Home from "./pages/Home";
@@ -18,7 +17,6 @@ const App = () => {
         <Route path="/login" element={<LogIn />} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/feeds" element={<Feeds />} />
-        <Route path="/addfeed" element={<AddFeed />} />
       </Routes>
     </>
   );
