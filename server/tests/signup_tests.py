a682ac6ae2859ba9eb8c0f4ef41797fbe4bcca66
ryantran <113406848+rryantran@users.noreply.github.com>
2024-08-03 00:08:46 -0500

    write tests for sign up route

---
+++ server/tests/signup_tests.py
@@ -0,0 +1,117 @@
+import pytest
+from werkzeug.security import generate_password_hash
+from app import create_app
+from config import TestConfig
+from exts import db
+from models import User
+
+
+@pytest.fixture
+def app():
+    app = create_app(TestConfig)
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    client = app.test_client()
+
+    return client
+
+
+def test_missing_username(client):
+    json = {'username': '', 'email': 'test_user@mail.com',
+            'password': 'password', 'confirm_password': 'password'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'All fields are required'
+
+
+def test_missing_email(client):
+    json = {'username': 'test_user', 'email': '',
+            'password': 'password', 'confirm_password': 'password'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'All fields are required'
+
+
+def test_missing_password(client):
+    json = {'username': 'test_user', 'email': 'test_user@mail.com',
+            'password': '', 'confirm_password': 'password'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'All fields are required'
+
+
+def test_missing_confirm_password(client):
+    json = {'username': 'test_user', 'email': 'test_user@mail.com',
+            'password': 'password', 'confirm_password': ''}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'All fields are required'
+
+
+def test_passwords_not_matching(client):
+    json = {'username': 'test_user', 'email': 'test_user@mail.com',
+            'password': 'password1', 'confirm_password': 'password2'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'Passwords do not match'
+
+
+def test_username_exists(client):
+    user = User(username='test_user', email='user@mail.com',
+                password=generate_password_hash('password'))
+
+    db.session.add(user)
+    db.session.commit()
+
+    json = {'username': 'test_user', 'email': 'test_user@mail.com',
+            'password': 'password', 'confirm_password': 'password'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'Username is already in use'
+
+
+def test_email_exists(client):
+    user = User(username='test_user', email='test_user@mail.com',
+                password=generate_password_hash('password'))
+
+    db.session.add(user)
+    db.session.commit()
+
+    json = {'username': 'user', 'email': 'test_user@mail.com',
+            'password': 'password', 'confirm_password': 'password'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 400
+    assert response.get_json()['message'] == 'Email is already in use'
+
+
+def test_valid_signup(client):
+    json = {'username': 'test_user', 'email': 'test_user@mail.com',
+            'password': 'password', 'confirm_password': 'password'}
+
+    response = client.post('/auth/signup', json=json)
+
+    assert response.status_code == 201
+    assert response.get_json()['message'] == 'User created successfully'
