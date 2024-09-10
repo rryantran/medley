a781867add113c65df5e836bf0e85e669f0fde53
ryantran <113406848+rryantran@users.noreply.github.com>
2024-09-10 02:10:03 -0500

    redo nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -14,26 +14,61 @@ const NavBar = () => {
 
   if (user) {
     return (
-      <div>
-        <Link to="/">medley</Link>
+      <div className="flex justify-between align-middle px-10 py-6 border-b border-silver">
+        <Link
+          to="/"
+          className="text-4xl underline decoration-8 decoration-pink"
+        >
+          medley
+        </Link>
 
-        <div>
-          <Link to="/articles">Articles</Link>
-          <Link to="/feeds">Feeds</Link>
-          <button onClick={handleLogOut}>Log Out</button>
+        <div className="flex gap-8 items-center text-lg">
+          <Link
+            to="/articles"
+            className="hover:underline decoration-2 decoration-pink"
+          >
+            Articles
+          </Link>
+
+          <Link
+            to="/feeds"
+            className="hover:underline decoration-2 decoration-pink"
+          >
+            Feeds
+          </Link>
+
+          <button
+            onClick={handleLogOut}
+            className="hover:underline decoration-2 decoration-pink"
+          >
+            Log Out
+          </button>
         </div>
       </div>
     );
   } else {
     return (
-      <div className="flex justify-between px-10 py-4">
-        <Link to="/" className="text-4xl">
+      <div className="flex justify-between align-middle px-10 py-6 border-b border-silver">
+        <Link
+          to="/"
+          className="text-4xl underline decoration-8 decoration-pink"
+        >
           medley
         </Link>
 
-        <div className="flex">
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Log In</Link>
+        <div className="flex gap-8 items-center text-lg">
+          <Link
+            to="/signup"
+            className="hover:underline decoration-2 decoration-pink"
+          >
+            Sign Up
+          </Link>
+          <Link
+            to="/login"
+            className="hover:underline decoration-2 decoration-pink"
+          >
+            Log In
+          </Link>
         </div>
       </div>
     );
